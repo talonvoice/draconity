e7539e8f8aaf1dc1436e110c93ed39d826503ec0
jcaw <40725916+jcaw@users.noreply.github.com>
2019-07-28 19:13:16 -0700

    Remove apple-specific code from shim.cpp
    It will be re-added at a later date, once the Windows codehook and symbolicator
    abstractions have been established.

--- src/shim.cpp
+++ src/shim.cpp
@@ -5,30 +5,20 @@
 #include <dlfcn.h>
 #include <fcntl.h>
 #include <libgen.h>
-#ifdef __APPLE__
-#include <mach-o/dyld.h>
-#endif //__APPLE__
 #include <pthread.h>
-#include <pwd.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
 
 #include "server.h"
 #include "draconity.h"
-#ifdef __APPLE__
-#include "CoreSymbolication.h"
-#endif //__APPLE__
 
 #ifndef streq
 #define streq(a, b) !strcmp(a, b)
 #endif
 
 // #define DEBUG
-
-static void *server_so = NULL, *mrec_so = NULL;
 
 #if CPP_PORT_IS_DONE // unclear whether draconity.h's _engine macro usage here is intentional - suspect not though
 drg_engine *_engine = NULL;
@@ -37,38 +27,6 @@ drg_engine *_engine = NULL;
 
 #include "api.h" // this defines the function pointers
 
-#define state draconity_state
-
-typedef struct code_hook {
-    struct code_hook **handle;
-    const char *name;
-    void *target;
-    uint64_t offset;
-    bool active;
-    uint8_t *addr, *patch, *orig;
-    size_t size;
-} code_hook;
-
-code_hook *DSXEngine_New_hook = NULL,
-          *DSXEngine_Create_hook = NULL,
-          *DSXEngine_LoadGrammar_hook = NULL,
-          *DSXEngine_GetMicState_hook = NULL;
-
-static void patch_write(void *addr, void *patch, size_t size) {
-    uint64_t prot_size = (size + 0xfff) & ~0xfff;
-    uint64_t prot_addr = (uintptr_t)addr &~0xfff;
-    mprotect((void *)prot_addr, size, PROT_READ|PROT_WRITE|PROT_EXEC);
-    memcpy(addr, patch, size);
-    mprotect((void *)prot_addr, size, PROT_READ|PROT_EXEC);
-}
-
-static void hook_apply(code_hook *hook) {
-    patch_write(hook->addr, hook->patch, hook->size);
-}
-
-static void hook_revert(code_hook *hook) {
-    patch_write(hook->addr, hook->orig, hook->size);
-}
 
 #if RUN_IN_DRAGON
 int draconity_set_param(const char *key, const char *value) {
@@ -177,49 +135,6 @@ static void engine_acquire(drg_engine *engine, bool early) {
     }
 }
 
-static void *DSXEngine_New() {
-    hook_revert(DSXEngine_New_hook);
-    drg_engine *engine = _DSXEngine_New();
-    hook_apply(DSXEngine_New_hook);
-    draconity_logf("DSXEngine_New() = %p", engine);
-    engine_acquire(engine, true);
-    return _engine;
-}
-
-static int DSXEngine_Create(char *s, uint64_t val, drg_engine **engine) {
-    hook_revert(DSXEngine_Create_hook);
-    int ret = _DSXEngine_Create(s, val, engine);
-    hook_apply(DSXEngine_Create_hook);
-    draconity_logf("DSXEngine_Create(%s, %llu, &%p) = %d", s, val, engine, ret);
-    engine_acquire(*engine, true);
-    return ret;
-}
-
-static int DSXEngine_GetMicState(drg_engine *engine, int64_t *state) {
-    engine_acquire(engine, false);
-    hook_revert(DSXEngine_GetMicState_hook);
-    int ret = _DSXEngine_GetMicState(engine, state);
-    hook_apply(DSXEngine_GetMicState_hook);
-    return ret;
-}
-
-static int DSXEngine_LoadGrammar(drg_engine *engine, int format, dsx_dataptr *data, drg_grammar **grammar) {
-    engine_acquire(engine, false);
-#ifdef DEBUG
-    printf("dumping grammar: %d\n", data->size);
-    uint8_t *buf = data->data;
-    for (int i = 0; i < data->size; i++) {
-        printf("%02x", buf[i]);
-    }
-    printf("\n\n"); fflush(stdout);
-#endif
-
-    hook_revert(DSXEngine_LoadGrammar_hook);
-    int ret = _DSXEngine_LoadGrammar(engine, format, data, grammar);
-    hook_apply(DSXEngine_LoadGrammar_hook);
-    return ret;
-}
-
 static int DSXEngine_SetBeginPhraseCallback() {
     draconity_logf("warning: called stubbed SetBeginPhraseCallback()\n");
     return 0;
@@ -303,147 +218,12 @@ int DSXGrammar_RegisterPhraseHypothesisCallback(drg_grammar *grammar, void *cb, 
 }
 */
 
-// patching code starts here
-static ZydisDecoder dis;
-static ZydisFormatter dis_fmt;
-static bool dis_ready = false;
-
-static void dis_ensure() {
-    if (!dis_ready) {
-        if (!ZYDIS_SUCCESS(ZydisDecoderInit(&dis, ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_ADDRESS_WIDTH_64)) ||
-                !ZYDIS_SUCCESS(ZydisFormatterInit(&dis_fmt, ZYDIS_FORMATTER_STYLE_INTEL))) {
-            printf("failed to init disassembler\n");
-            abort();
-        }
-        dis_ready = true;
-    }
-}
-
-static void dis_mem(void *addr, size_t size) {
-    dis_ensure();
-    size_t offset = 0;
-    ZydisDecodedInstruction ins;
-    char buf[256];
-    while (ZYDIS_SUCCESS(ZydisDecoderDecodeBuffer(&dis, addr + offset, size - offset, (uint64_t)addr + offset, &ins))) {
-        ZydisFormatterFormatInstruction(&dis_fmt, &ins, buf, sizeof(buf));
-        printf("%016llx %s\n", ins.instrAddress, buf);
-        offset += ins.length;
-    }
-}
-
-static size_t dis_code_size(void *addr, size_t size) {
-    dis_ensure();
-    size_t offset = 0;
-    ZydisDecodedInstruction ins;
-    while (ZYDIS_SUCCESS(ZydisDecoderDecodeBuffer(&dis, addr + offset, 64, (uint64_t)addr + offset, &ins)) && offset < size) {
-        offset += ins.length;
-    }
-    return offset;
-}
-
-static void patch_stub(void *addr, void *to) {
-    uint64_t *target = (void *)(addr + 6 + *(uint32_t *)(addr + 2));
-    *target = (uintptr_t)to;
-}
-
-#define JMP_SIZE 12
-static void fill_jmp(void *buf, void *addr) {
-    uint8_t jmp[12] = {0x48, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xe0};
-    *(uint64_t *)&jmp[2] = (uintptr_t)addr;
-    memcpy(buf, jmp, sizeof(jmp));
-}
-
-static void patch_redirect(void *addr, void *to, code_hook *hook) {
-    hook->size = dis_code_size(addr, JMP_SIZE);
-    hook->orig = malloc(hook->size);
-    memcpy(hook->orig, addr, hook->size);
-
-    hook->patch = malloc(hook->size);
-    fill_jmp(hook->patch, to);
-    for (int i = JMP_SIZE; i < hook->size; i++) {
-        hook->patch[i] = 0x90;
-    }
-    hook->addr = addr;
-    hook_apply(hook);
-}
 
 typedef struct {
     const char *name;
     void **ptr;
     bool active;
 } symload;
-
-#ifdef __APPLE__
-static void walk_image(CSSymbolicatorRef csym, const char *image, code_hook *hooks, symload *loads) {
-    CSSymbolOwnerRef owner = CSSymbolicatorGetSymbolOwnerWithNameAtTime(csym, image, kCSNow);
-    if (CSIsNull(owner)) {
-        printf("  [!] IMAGE NOT FOUND: %s\n", image);
-        return;
-    }
-    CSSymbolOwnerForeachSymbol(owner, ^int (CSSymbolRef sym) {
-        const char *name = CSSymbolGetName(sym);
-        if (name) {
-            CSRange range = CSSymbolGetRange(sym);
-            const char *stub = "DYLD-STUB$$";
-            bool is_stub = (strncmp(name, stub, strlen(stub)) == 0);
-            if (is_stub) {
-                name += strlen(stub);
-            }
-            code_hook *hook = hooks;
-            while (hook && hook->name) {
-                if (strcmp(name, hook->name) == 0) {
-                    if (range.size < 12 && !is_stub) {
-                        printf("[!] skipping %s hook: not enough space (%llu < 12)\n", name, range.size);
-                    } else {
-                        uint8_t *addr = (void *)range.addr;
-                        addr += hook->offset;
-                        if (hook->offset) {
-                            printf("[+] hooking %s+%#llx (%p)\n", name, hook->offset, addr);
-                        } else {
-                            printf("[+] hooking %s (%p)\n", name, addr);
-                        }
-                        if (is_stub) {
-                            patch_stub(addr, hook->target);
-                        } else {
-                            patch_redirect(addr, hook->target, hook);
-                        }
-                        if (hook->handle) {
-                            *hook->handle = hook;
-                        }
-                        hook->active = true;
-                    }
-                }
-                hook++;
-            }
-            symload *load = loads;
-            while (load && load->name) {
-                if (strcmp(name, load->name) == 0) {
-                    *load->ptr = (void *)range.addr;
-                    printf("%46s = %p\n", name, *load->ptr);
-                    load->active = true;
-                }
-                load++;
-            }
-            return 0;
-        }
-        return 1;
-    });
-    code_hook *hook = hooks;
-    while (hook && hook->name) {
-        if (!hook->active) {
-            printf("[!] Failed to hook %s\n", hook->name);
-        }
-        hook++;
-    }
-    symload *load = loads;
-    while (load && load->name) {
-        if (!load->active) {
-            printf("[!] Failed to load %s\n", load->name);
-        }
-        load++;
-    }
-}
-#endif //__APPLE__
 
 #define h(_name) {.handle=NULL, .name=#_name, .target=_name, .offset=0, .active=false, 0}
 #define ho(_name, _handle) {.handle=_handle, .name=#_name, .target=_name, .offset=0, .active=false, 0}
@@ -532,27 +312,6 @@ static symload mrec_syms[] = {
 
 static void *draconity_install(void *_) {
     printf("[+] draconity starting\n");
-
-#ifdef __APPLE__
-    char *image = getenv("DRACONITY_IMAGE");
-    if (!image) {
-        char path[MAXPATHLEN];
-        uint32_t size = MAXPATHLEN;
-        if (_NSGetExecutablePath(path, &size)) {
-            printf("[!] failed to find executable\n");
-            return NULL;
-        }
-        image = basename(path);
-    }
-    CSSymbolicatorRef csym = CSSymbolicatorCreateWithTask(mach_task_self());
-    walk_image(csym, "server.so", NULL, server_syms);
-    walk_image(csym, "mrec.so", NULL, mrec_syms);
-    walk_image(csym, image, dragon_hooks, NULL);
-    CSRelease(csym);
-    if (!_engine) {
-        printf("[+] waiting for engine\n");
-    }
-#endif //__APPLE__
     draconity_init();
     return NULL;
 }
